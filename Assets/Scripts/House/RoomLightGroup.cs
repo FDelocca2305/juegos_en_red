29b9ccd04fac7c945da36b51a2b3b6a9efd5d7b5
FDelocca2305 <franco.delocca@gmail.com>
2025-10-18 21:05:11 -0300

    added lights switch materials to emissor on and off

--- Assets/Scripts/House/RoomLightGroup.cs
+++ Assets/Scripts/House/RoomLightGroup.cs
@@ -8,8 +8,21 @@ public class RoomLightGroup : MonoBehaviour
     [Serializable]
     public class LightEntry
     {
+        [Header("Light")]
         public Light light;
+
+        [Header("Emissive (opcional)")]
+        public Renderer emissiveRenderer;
+        public int emissiveMaterialIndex = 0;
+        [ColorUsage(true, true)] public Color onEmission = Color.white;
+        [ColorUsage(true, true)] public Color offEmission = Color.black;
+        public bool affectRealtimeGI = false;
+
+        [NonSerialized] public MaterialPropertyBlock mpb;
+        [NonSerialized] public bool initialized;
     }
+
+    private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
 
     [SerializeField] private List<LightEntry> entries = new();
     [SerializeField] private bool startOn = true;
@@ -20,6 +33,18 @@ public class RoomLightGroup : MonoBehaviour
     void Awake()
     {
         IsOn = startOn;
+        foreach (var e in entries)
+        {
+            if (e.emissiveRenderer)
+            {
+                var mats = e.emissiveRenderer.sharedMaterials;
+                if (e.emissiveMaterialIndex >= 0 && e.emissiveMaterialIndex < mats.Length && mats[e.emissiveMaterialIndex])
+                {
+                    if (e.onEmission == default)
+                        e.onEmission = mats[e.emissiveMaterialIndex].GetColor(EmissionColor);
+                }
+            }
+        }
         ApplyState(IsOn);
     }
 
@@ -38,6 +63,19 @@ public class RoomLightGroup : MonoBehaviour
         foreach (var e in entries)
         {
             if (e.light) e.light.enabled = on;
+
+            if (e.emissiveRenderer)
+            {
+                if (e.mpb == null) e.mpb = new MaterialPropertyBlock();
+
+                var color = on ? e.onEmission : e.offEmission;
+                e.emissiveRenderer.GetPropertyBlock(e.mpb, e.emissiveMaterialIndex);
+                e.mpb.SetColor(EmissionColor, color);
+                e.emissiveRenderer.SetPropertyBlock(e.mpb, e.emissiveMaterialIndex);
+                
+                if (e.affectRealtimeGI)
+                    DynamicGI.SetEmissive(e.emissiveRenderer, color);
+            }
         }
     }
 }
